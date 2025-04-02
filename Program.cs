c042e08e706b3cd18d1d127ef3f8a281d9ceda9d
Mgnidenko <134139841+Mgnidenko@users.noreply.github.com>
2025-04-02 22:04:00 +0600

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -66,7 +66,7 @@ namespace secontProj
 
             }
 
-
+            // заменить на enum
             if (checkNumber == 1)
             {
                 Console.WriteLine(Encoder(userWord, isNumberEncoderAndDecoder));
